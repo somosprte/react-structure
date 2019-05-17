49110d23f9e46407684da3d9780be6c5a65a0155
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2019-05-17 14:37:08 -0300

    Installed Sucrase

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -452,6 +452,16 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            {
+              test: /\.js$/,
+              use: {
+                loader: '@sucrase/webpack-loader',
+                options: {
+                  transforms: ['jsx']
+                }
+              }
+            },
+
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
