063334ca68b93aba6970b50b112241a3bd8d8f88
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-10-29 15:18:35 -0300

    fixed menu

--- src/layouts/Default/components/Header/components/Menu/index.js
+++ src/layouts/Default/components/Header/components/Menu/index.js
@@ -15,47 +15,29 @@ function Menu(props) {
   return (
     <Nav>
       <Title active={active === ''}>
-        <Link to="/">Dashboard</Link>
+        <Link to="/">Repositórios</Link>
       </Title>
 
       <Title>
-        <span>Ecommerce</span>
+        <span>Exemplo</span>
 
         <ul>
           <Item>
-            <Link /* active={active === ''} */ to="/">Categorias</Link>
+            <Link to="/">Categorias</Link>
           </Item>
 
           <Item>
-            <Link /* active={active === ''} */ to="/">Campanhas</Link>
+            <Link to="/">Campanhas</Link>
           </Item>
 
           <Item>
-            <Link /* active={active === ''} */ to="/">Produtos</Link>
+            <Link to="/">Produtos</Link>
           </Item>
 
           <Item>
-            <Link /* active={active === ''} */ to="/">Pedidos</Link>
+            <Link to="/">Pedidos</Link>
           </Item>
         </ul>
-      </Title>
-
-      <Title>
-        <span>Conteúdo</span>
-
-        <ul>
-          <Item>
-            <Link /* active={active === ''} */ to="/">Banners</Link>
-          </Item>
-
-          <Item>
-            <Link /* active={active === ''} */ to="/">Templates de e-mail</Link>
-          </Item>
-        </ul>
-      </Title>
-
-      <Title /* active={active === ''} */>
-        <Link to="/">Eventos</Link>
       </Title>
     </Nav>
   );
