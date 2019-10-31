5c7643a406378f5ac06494caf1ebddf2876e79ec
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-10-31 08:36:47 -0300

    fixed input component

--- src/components/Form/Input/index.js
+++ src/components/Form/Input/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, StyledInput, Icon } from './styles';
 
 function Input(props) {
-  const { icon, responsive, cvv } = props;
+  const { icon } = props;
 
   return (
     <Container>
@@ -11,18 +12,23 @@ function Input(props) {
         <StyledInput {...props} />
       </div>
 
-      {icon && (
-        <Icon cvv={cvv} responsive={responsive}>
-          {icon}
-        </Icon>
-      )}
+      {icon !== null && <Icon>{icon}</Icon>}
     </Container>
   );
 }
 
 Input.defaultProps = {
-  responsive: false,
-  icon: false,
+  name: '',
+  placeholder: '',
+  icon: null,
+  onChange: () => {},
+};
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  icon: PropTypes.element,
+  onChange: PropTypes.func,
 };
 
 export default Input;
--- stories/4-Input.stories.js
+++ stories/4-Input.stories.js
@@ -1,28 +1,22 @@
 import React from 'react';
 
+import { action } from '@storybook/addon-actions';
+
 import { Form } from 'components';
 import { Search } from 'assets/icons';
 import { colors } from 'assets/styles';
-import CVV from 'assets/images/cvc.png';
 
 export default {
   title: 'Form|Input',
 };
 
-export const standard = () => (
-  <Form>
-    <Form.Input name="standard" placeholder="standard" />
-  </Form>
-);
-
-export const withImage = () => (
-  <Form>
-    <Form.Input cvv name="cvv" placeholder="CVV" maxLength="4" icon={<img alt="cvv" src={CVV} />} />
-  </Form>
-);
+export const standard = () => <Form.Input name="standard" placeholder="Standard" onChange={action('changed input')} />;
 
 export const withIcon = () => (
-  <Form>
-    <Form.Input name="search" placeholder="O que você busca?" icon={<Search color={colors.black} />} />
-  </Form>
+  <Form.Input
+    name="search"
+    placeholder="O que você busca?"
+    icon={<Search color={colors.black} />}
+    onChange={action('changed input')}
+  />
 );
