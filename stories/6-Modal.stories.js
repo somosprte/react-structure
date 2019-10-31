748e22c1fa6a08391754f5013e0c1ed0302f6e5f
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-10-31 08:19:33 -0300

    added close modal action

--- stories/6-Modal.stories.js
+++ stories/6-Modal.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { action } from '@storybook/addon-actions';
+
 import { Modal } from 'components';
 
 export default {
@@ -7,7 +9,7 @@ export default {
 };
 
 export const standard = () => (
-  <Modal visible={true} setVisible={() => {}}>
+  <Modal visible={true} setVisible={action('close Modal')}>
     <div>
       <h1>some text on modal</h1>
     </div>
